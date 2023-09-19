be1c53739d38d482801079b49e29eb0c5f16780d
ezequiel sousa baldez <ezequielbaldez1@gmail.com>
2023-09-19 20:41:50 -0300

    Atualiza verção welton

--- iMyApp/Apresentacao/WindowsForms/Program.cs
+++ iMyApp/Apresentacao/WindowsForms/Program.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using WindowsForms.Telas.Cargos;
+
 namespace WindowsForms
 {
     internal static class Program
@@ -8,10 +11,21 @@ namespace WindowsForms
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            try
+            {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MDIPrincipal());
+            }
+            catch (Exception ex)
+            {
+                //Log it
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
